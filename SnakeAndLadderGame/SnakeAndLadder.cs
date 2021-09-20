b94e410bbd388bdf6635860f9e1fa9af4ece1f47
Mabel <maybelchristina@gmail.com>
2021-09-20 21:27:27 +0530

    [Mabel] Add : UC3 added

--- SnakeAndLadderGame/SnakeAndLadder.cs
+++ SnakeAndLadderGame/SnakeAndLadder.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,12 +6,35 @@ namespace SnakeAndLadderGame
 {
     class SnakeAndLadder
     {
-        int playerposition = 0;
+        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        int playerposition;
         public void Play()
         {
             Random random = new Random();
-            int number = random.Next(1, 7);
-            Console.WriteLine("Player rolled the dice and got " + number + " number");
+            int rollDie = random.Next(1, 7);
+            int options = random.Next(0, 3);
+            switch (options)
+            {
+                case NO_PLAY:
+                    Console.WriteLine("NO_PLAY");
+                    playerposition = 0;
+                    break;
+                case LADDER:
+                    Console.WriteLine("LADDER");
+                    playerposition += rollDie;
+                    break;
+                case SNAKE:
+                    Console.WriteLine("SNAKE");
+                    playerposition -= rollDie;
+                    if (playerposition < 0)
+                    {
+                        playerposition = 0;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine("Player is at " + playerposition);
         }
     }
 }

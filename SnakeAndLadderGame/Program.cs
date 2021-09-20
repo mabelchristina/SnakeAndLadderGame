e45241833877831a2dc64366c3604612c4aebc82
Mabel <maybelchristina@gmail.com>
2021-09-20 21:16:56 +0530

    [Mabel] Add : UC1 added

---
+++ SnakeAndLadderGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadderGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I have Intialized player position = 0");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SnakeAndLadderGame/SnakeAndLadder.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadderGame
+{
+    class SnakeAndLadder
+    {
+        int playerposition = 0;
+    }
+}
+
